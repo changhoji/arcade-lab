8d0b75284e85190d8cbc7dd2cd27b778b2b30f33
changhoji <onkkno1@gmail.com>
2025-11-17 00:48:52 +0900

    feat: add animation of moving character

--- client/Assets/Scripts/PlayerController.cs
+++ client/Assets/Scripts/PlayerController.cs
@@ -8,12 +8,15 @@ public class PlayerController : MonoBehaviour
     [Inject] LobbyManager m_LobbyManager;
     [SerializeField] float m_MoveSpeed = 5f;
     Rigidbody2D m_Rigidbody;
+    Animator m_Animator;
+    SpriteRenderer m_SpriteRenderer;
     Vector2 m_MoveInput;
 
     public string UserId;
     public bool IsOwner = false;
 
     float m_LastSendTime;
+    Vector3 m_PreviousPosition;
 
     public void UpdateRemotePosition(float x, float y)
     {
@@ -33,7 +36,10 @@ public class PlayerController : MonoBehaviour
     void Awake()
     {
         m_Rigidbody = GetComponent<Rigidbody2D>();
+        m_Animator = GetComponent<Animator>();
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
         m_LastSendTime = Time.time;
+        m_PreviousPosition = transform.position;
     }
 
     void Start()
@@ -47,19 +53,43 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if (!IsOwner) return;
+        if (!IsOwner)
+        {
+            if(transform.position != m_PreviousPosition)
+            {
+                m_Animator.SetBool("Walking", true);
+                m_SpriteRenderer.flipX = transform.position.x < m_PreviousPosition.x;
+            }
+            else
+            {
+                m_Animator.SetBool("Walking", false);
+            }
+            m_PreviousPosition = transform.position;
+            return;
+        }
+
         m_MoveInput = new(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
         if (m_MoveInput != Vector2.zero && Time.time - m_LastSendTime > 0.01f)
         {
             m_LastSendTime = Time.time;
             SendPosition();
+            m_Animator.SetBool("Walking", true);
+        }
+        else
+        {
+            m_Animator.SetBool("Walking", false);
         }
     }
 
     void FixedUpdate()
     {
         m_Rigidbody.linearVelocity = m_MoveInput * m_MoveSpeed;
+        if (m_Rigidbody.linearVelocityX != 0)
+        {
+            m_SpriteRenderer.flipX = m_Rigidbody.linearVelocityX < 0;    
+        }
+        
     }
 
     void SendPosition()
