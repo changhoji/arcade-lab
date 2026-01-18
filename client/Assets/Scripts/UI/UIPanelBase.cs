aca8e9bdc823aada36ea872b0eb0a8fa42fd9d7f
changhoji <onkkno1@gmail.com>
2026-01-18 15:34:00 +0900

    refactor: add panel settings and change uss files

--- client/Assets/Scripts/UI/UIPanelBase.cs
+++ client/Assets/Scripts/UI/UIPanelBase.cs
@@ -7,6 +7,10 @@ public abstract class UIPanelBase : MonoBehaviour
     public event Action OnShowPanel;
     public event Action OnHidePanel;
 
+    [SerializeField] protected bool m_CloseOnEscape = true;
+    [SerializeField] protected bool m_PreventMovement = true;
+    [SerializeField] protected bool m_ModalBackground = true;
+
     protected UIDocument m_UIDocument;
     protected VisualElement m_Root;
     protected float m_ShownTime;
@@ -30,7 +34,7 @@ public abstract class UIPanelBase : MonoBehaviour
             return;
         }
 
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (m_CloseOnEscape && Input.GetKeyDown(KeyCode.Escape))
         {
             Hide();
         }
@@ -39,19 +43,29 @@ public abstract class UIPanelBase : MonoBehaviour
     public virtual void Show()
     {
         var localPlayer = PlayerBase.LocalPlayer;
-        if (localPlayer != null)
+        if (m_PreventMovement && localPlayer != null)
         {
             localPlayer.GetComponent<PlayerMovement>().IsMovable = false;
         }
         m_Root.style.display = DisplayStyle.Flex;
         m_ShownTime = Time.time;
+
+        if (m_ModalBackground)
+        {
+            m_Root.AddToClassList("dimmed");
+        }
+        else
+        {
+            m_Root.RemoveFromClassList("dimmed");
+        }
+
         OnShowPanel?.Invoke();
     }
 
     public virtual void Hide()
     {
         var localPlayer = PlayerBase.LocalPlayer;
-        if (localPlayer != null)
+        if (m_PreventMovement && localPlayer != null)
         {
             localPlayer.GetComponent<PlayerMovement>().IsMovable = true;
         }
